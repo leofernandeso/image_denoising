4eb3cb10e2a757ef92792dfb9d0379e98efc0020
Leonardo Fernandes Oliveira <leonardo.oliveira@tum.de>
2023-02-05 22:55:05 +0100

    Add noise models that add noise to a tf tensor

--- noise_models.py
+++ noise_models.py
@@ -5,9 +5,11 @@ from typing import Callable
 NoiseAdderCallable = Callable[[tf.Tensor], tf.Tensor]
 
 
-def add_gaussian_noise(tensor: tf.Tensor, sigma: float) -> tf.Tensor:
+def add_gaussian_noise(
+    tensor: tf.Tensor, sigma: float, normalize: float = 1 / 255.0
+) -> tf.Tensor:
     """
     Adds gaussian noise to tensor and returns the result
     """
-    noise = tf.random.normal(tensor.shape, stddev=sigma)
+    noise = tf.random.normal(tensor.shape, stddev=sigma * normalize)
     return tensor + noise
