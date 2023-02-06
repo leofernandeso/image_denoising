d82a4b6906f7865da83851d2c9f0f50dc53d16b9
Leonardo Fernandes Oliveira <leonardo.oliveira@tum.de>
2023-02-06 02:45:46 +0100

    visualize outputs when visualizing batch

--- visualization.py
+++ visualization.py
@@ -2,13 +2,15 @@ import tensorflow as tf
 from matplotlib import pyplot as plt
 
 
-def visualize_batch(x_batch, y_batch):
+def visualize_batch(x_batch, y_batch, output_batch, **kwargs):
     """Converts x_batch and y_batch to numpy arrays and plots them in a grid"""
     x_batch = x_batch.numpy()
     y_batch = y_batch.numpy()
-    fig, axes = plt.subplots(2, 4, figsize=(10, 5))
+    output_batch = output_batch.numpy()
+    fig, axes = plt.subplots(3, 4, figsize=(10, 5))
     for i in range(x_batch.shape[0]):
+        print(x_batch[i], y_batch[i])
         axes[0, i].imshow(x_batch[i].astype("float32"))
         axes[1, i].imshow(y_batch[i].astype("float32"))
+        axes[2, i].imshow(output_batch[i].astype("float32"))
     plt.show()
-
