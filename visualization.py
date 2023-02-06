e46260945b219d0e1054c46a23b3a13bcb5ec01c
Leonardo Fernandes Oliveira <leonardo.oliveira@tum.de>
2023-02-06 15:44:05 +0100

    check type in visualize_batch

--- visualization.py
+++ visualization.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 from matplotlib import pyplot as plt
 
@@ -6,7 +7,8 @@ def visualize_batch(x_batch, y_batch, output_batch, **kwargs):
     """Converts x_batch and y_batch to numpy arrays and plots them in a grid"""
     x_batch = x_batch.numpy()
     y_batch = y_batch.numpy()
-    output_batch = output_batch.numpy()
+    if not isinstance(output_batch, np.ndarray):
+        output_batch = output_batch.numpy()
     fig, axes = plt.subplots(3, 4, figsize=(10, 5))
     for i in range(x_batch.shape[0]):
         print(x_batch[i], y_batch[i])
